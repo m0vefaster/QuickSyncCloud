3aac8fe8ddad06614abff6fbfe42f79db36edff1
vish1562 <vishal1562@gmail.com>
2015-03-04 22:32:12 -0800

    Removed Extra Classes

--- Sync.java
+++ Sync.java
@@ -8,8 +8,6 @@ import java.net.*;
 
 public class Sync implements Runnable{
     ListOfFiles files;
-    SendList toBeSent;
-    PeerFileList fileList;
     ListOfPeers listOfPeers;
     
     Sync(ListOfPeers listOfPeers)
