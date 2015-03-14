be5b5c5e90c504008b74ad15e1caf919e1af761a
Ubuntu <ubuntu@ip-172-31-29-24.us-west-2.compute.internal>
2015-03-14 04:16:30 +0000

    Added getList2 for new files

--- ListOfFiles.java
+++ ListOfFiles.java
@@ -8,6 +8,7 @@ class ListOfFiles implements Serializable
     static String folder = "QuickSync";
     static String path = homeDir + "/" + folder ;
     ArrayList<String> list;
+    ArrayList<String> list2;
     
     ListOfFiles( )
     {
@@ -37,11 +38,11 @@ class ListOfFiles implements Serializable
         
         for(int i=0;i< listOfFiles.length;i++)
         {
-            if(listOfFiles[i].isFile()  && listOfFiles[i].getName().charAt(0) != '.')
+            if(listOfFiles[i].isFile()  && listOfFiles[i].getName().charAt(0) != '.' && listOfFiles[i].length()!=0)
             {
                 list.add(path+"/"+ listOfFiles[i].getName());
             }
-            else if(listOfFiles[i].isDirectory() && listOfFiles[i].getName().charAt(0) != '.')
+            else if(listOfFiles[i].isDirectory() && listOfFiles[i].getName().charAt(0) != '.' && listOfFiles[i].length()!=0)
             {
                 getListHelper(list,path+"/"+ listOfFiles[i].getName());
             }
@@ -50,6 +51,33 @@ class ListOfFiles implements Serializable
         return list;
     }
     
+    
+    ArrayList<String> getList2 ( )
+    {
+         list2 = new ArrayList<String>();
+         return removeAbsolutePath(getListHelper(list2,path));
+    }
+
+    ArrayList<String> getListHelper2 (ArrayList<String> list2, String path)
+    {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for(int i=0;i< listOfFiles.length;i++)
+        {
+            if(listOfFiles[i].isFile()  && listOfFiles[i].getName().charAt(0) != '.')
+            {
+                list.add(path+"/"+ listOfFiles[i].getName());
+            }
+            else if(listOfFiles[i].isDirectory() && listOfFiles[i].getName().charAt(0) != '.')
+            {
+                getListHelper2(list,path+"/"+ listOfFiles[i].getName());
+            }
+        }
+
+        return list2;
+    }
+	
     void setList(ArrayList<String> list){
         this.list = list;
     }
